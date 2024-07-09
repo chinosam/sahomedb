82da1fcfc6b82b7bdfe0d6b3f53ddf853dcb4f79
obaraelijah <elijahobara357@gmail.com>
2024-07-09 21:09:21 +0300

    fix(tests): fix rare connection reset error

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,18 +1,22 @@
 use sahomedb::db::server::Server;
 use tokio::runtime::Runtime;
 
-pub const HOST: &str = "127.0.0.1";
-pub const PORT: &str = "31415";
-
-pub async fn run_server() -> Runtime {
+pub async fn run_server() -> (Runtime, String) {
     let runtime = Runtime::new().unwrap();
 
+    // Generate a random port: 314xx.
+    // This is needed to run multiple tests in parallel and
+    // prevent connection reset error when testing.
+    let random_number = rand::random::<u16>() % 100 + 31400;
+    let port = random_number.to_string();
+    let _port = port.clone();
+
     runtime.spawn(async move {
-        let server = Server::new(HOST, PORT).await;
+        let server = Server::new("127.0.0.1", _port.as_str()).await;
         server.serve().await;
     });
 
-    runtime
+    (runtime, port)
 }
 
 pub async fn stop_server(runtime: Runtime) {
--- tests/test_servers.rs
+++ tests/test_servers.rs
@@ -2,11 +2,19 @@ mod common;
 
 use common::*;
 
+const HOST: &str = "http://127.0.0.1";
+
+// JSON body to create a new key-value store.
+const CREATE_KVS: &str = r#"{
+    "key": "test_key",
+    "value": {"embedding": [0.0], "data": {}}
+}"#;
+
 #[tokio::test]
 async fn test_get_root() {
-    let runtime = run_server().await;
+    let (runtime, port) = run_server().await;
 
-    let url = format!("http://{}:{}", HOST, PORT);
+    let url = format!("{}:{}", HOST, port);
     let res = reqwest::get(url).await.unwrap();
 
     assert_eq!(res.status(), 200);
@@ -16,18 +24,12 @@ async fn test_get_root() {
 
 #[tokio::test]
 async fn test_post_kvs() {
-    let runtime = run_server().await;
-
-    let url = format!("http://{}:{}/kvs", HOST, PORT);
-
-    let json = r#"{
-        "key": "test_key",
-        "value": {"embedding": [0.0], "data": {}}
-    }"#;
+    let (runtime, port) = run_server().await;
 
     // Make a post request.
+    let url = format!("{}:{}/kvs", HOST, port);
     let client = reqwest::Client::new();
-    let res = client.post(&url).body(json).send().await;
+    let res = client.post(&url).body(CREATE_KVS).send().await;
 
     // Get the response code.
     let code = if res.is_ok() {
