dd90c68e725e6813e4b104de43e43ad0ff725b2e
obaraelijah <elijahobara357@gmail.com>
2024-07-09 16:34:07 +0300

    feat: add error handler for set get

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,2 +1,26 @@
 pub mod client;
 pub mod server;
+
+use std::collections::HashMap;
+use serde_json::to_string;
+
+pub struct ErrorResponse {
+    pub code: String,
+    pub message: String,
+}
+
+impl ErrorResponse {
+    pub fn new(code: &str, message: &str) -> ErrorResponse {
+        ErrorResponse {
+            code: code.to_string(),
+            message: message.to_string(),
+        }
+    }
+
+    pub fn response(&self) -> String {
+        let mut map = HashMap::new();
+        map.insert("status", self.code.clone());
+        map.insert("message", self.message.clone());
+        to_string(&map).unwrap()
+    }
+}
