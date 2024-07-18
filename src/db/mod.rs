a8525a58e88b12467757185eb5b8c2647e679040
obaraelijah <elijahobara357@gmail.com>
2024-07-18 10:45:09 +0300

    chore: reset all project files

--- src/db/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod routes;
-pub mod server;
-mod utils;
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
 use dotenv::dotenv;
-use sahomedb::db::server::Config;
-use sahomedb::serve;
-use std::env;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     dotenv().ok();
-
-    let port = env::var("SAHOMEDB_PORT").unwrap_or(String::from("3141"));
-
-    let dimension = env_get_dimension();
-
-    let config = {
-        let token = get_env("SAHOMEDB_TOKEN");
-        let path = "data".to_string();
-        Config { dimension, token, path }
-    };
-
-    println!("SahomeDB is running on port {}.", port);
-    println!("SahomeDB accepts embeddings of {} dimension.", dimension);
-
-    let host = "0.0.0.0";
-    serve(host, &port, config).await;
-}
-
-fn env_get_dimension() -> usize {
-    get_env("SAHOMEDB_DIMENSION")
-        .parse::<usize>()
-        .expect("variable 'SAHOMEDB_DIMENSION' must be an integer")
-}
-
-fn get_env(key: &str) -> String {
-    let not_set = format!("env variable '{}' required", key);
-    env::var(key).expect(&not_set)
 }
