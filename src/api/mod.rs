4ece63ef4cee3b0981803068add347a5358a0bb8
obaraelijah <elijahobara357@gmail.com>
2024-07-18 11:51:16 +0300

    feat: add auth and error catchers

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,4 +1,8 @@
 use crate::db::database::*;
+use crate::get_env;
+use rocket::http::Status;
+use rocket::request::*;
+use rocket::Request;
 use serde::Serialize;
 use std::collections::HashMap;
 
@@ -30,5 +34,31 @@ impl Response {
     pub fn from<Value: Serialize>(value: Value) -> Response {
         let body = serde_json::to_string(&value).unwrap();
         Response(body)
+    }
+}
+
+pub struct Auth {
+    pub token: String,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Auth {
+    type Error = &'static str;
+
+    async fn from_request(
+        request: &'r Request<'_>,
+    ) -> Outcome<Self, Self::Error> {
+        let server_token = get_env("SAHOMEDB_TOKEN");
+        let token = request.headers().get_one("x-sahomedb-token");
+
+        if token.is_none() || token.unwrap() != server_token {
+            return Outcome::Error((
+                Status::Unauthorized,
+                "Invalid x-sahomedb-token header.",
+            ));
+        }
+
+        let token = token.unwrap().to_string();
+        Outcome::Success(Auth { token })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,28 @@ use sahomedb::db::database::*;
 use sahomedb::*;
 
 use dotenv::dotenv;
+use rocket::http::Status;
 
 #[macro_use]
 extern crate rocket;
+
+#[catch(404)]
+fn catch_404() -> (Status, Response) {
+    let message = "Invalid endpoint or method.";
+    (Status::NotFound, Response::error(message))
+}
+
+#[catch(401)]
+fn catch_401() -> (Status, Response) {
+    let message = "Invalid x-oasysdb-token header.";
+    (Status::Unauthorized, Response::error(message))
+}
 
 #[launch]
 fn rocket() -> _ {
     dotenv().ok();
 
+    let _token = get_env("SAHOMEDB_TOKEN");
     let dimension = env_get_dimension();
 
     let config = {
@@ -28,6 +42,7 @@ fn rocket() -> _ {
         .mount("/", routes![get_status, get_version])
         .mount("/values", routes![set_value, get_value, delete_value])
         .mount("/graphs", routes![create_graph, delete_graph, query_graph])
+        .register("/", catchers![catch_401, catch_404])
 }
 
 fn env_get_dimension() -> usize {
