621d0e4b0363399311ca94aa2b234e984754708e
obaraelijah <elijahobara357@gmail.com>
2024-07-22 10:20:40 +0300

    chore: reset lsh indexing algorithm

--- src/ix/mod.rs
+++ src/ix/mod.rs
@@ -1,17 +1,1 @@
-mod hyperplane;
-mod tree;
-
 pub mod index;
-pub mod vector;
-
-use hyperplane::*;
-use tree::*;
-
-// External deps.
-use dashmap::DashSet;
-use itertools::Itertools;
-use rand::prelude::SliceRandom;
-use std::cmp::min;
-use std::collections::HashMap;
-
-pub use vector::*;
