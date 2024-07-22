5d0028f05fd097847c74570d6bd61c844497f6d4
obaraelijah <elijahobara357@gmail.com>
2024-07-22 15:53:42 +0300

    docs: improve readme and inline docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-//! ![Sahome](https://i.postimg.cc/X7rGVsBb/banner.png)
+#![warn(missing_docs)]
+#![doc = include_str!("../readme.md")]
+#![doc(html_favicon_url = "https://i.postimg.cc/W3T230zk/favicon.png")]
+#![doc(html_logo_url = "https://i.postimg.cc/Vv0HPVwB/logo.png")]
 
 mod db;
 mod func;
